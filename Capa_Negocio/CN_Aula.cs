77ef0624f07d167de93296bffdd74f9e01ade895
Valeria705 <89555606+Valeria705@users.noreply.github.com>
2022-07-07 14:37:46 -0500

    Add files via upload

---
+++ Capa_Negocio/CN_Aula.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Entidad;
+using Capa_Datos;
+using System.Data;
+
+namespace Capa_Negocio
+{
+    public class CN_Aula
+    {
+        CD_Aula capaDatos = new CD_Aula();
+
+        public bool guardar_Aula(CE_Aula Aula)
+        {
+            return capaDatos.guardar_Aula(Aula);
+        }
+
+        public bool modificar_Aula(CE_Aula Aula)
+        {
+            return capaDatos.modificar_Aula(Aula);
+        }
+
+        public bool eliminar_Aula(string Id)
+        {
+            return capaDatos.eliminar_Aula(Id);
+        }
+
+        /*public bool anula_Aulas(CE_Aula conect5)
+        {
+            return nuevoAula.anula_Aulas(conect5);
+        }*/
+
+        public CE_Aula consultar_Aula(string Id)
+        {
+            return capaDatos.consultar_Aula(Id);
+        }
+
+        public List<CE_Aula> consultar_Aulas()
+        {
+            return capaDatos.consultar_Aulas();
+        }
+    }
+}
